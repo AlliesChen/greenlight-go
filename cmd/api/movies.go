fca263a5b1111a2fa7d87d1628000bf3686b0fb7
Yu-Pang <tedchen524@gmail.com>
2025-04-21 23:51:58 +0800

    ✨ Advanced JSON customization

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/AlliesChen/greenlight-go/internal/data"
 )
 
 // POST /v1/movies
@@ -13,8 +16,21 @@ func (app *application) craeteMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIdParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
-	fmt.Fprint(w, "show the movie details of movie %d\n", id)
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Inception",
+		Runtime:   102,
+		Genres:    []string{"Action", "Sci-Fi"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
