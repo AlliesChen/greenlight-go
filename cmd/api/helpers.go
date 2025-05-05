8c6d1965ac4bde1733753be8dc6e010981d96819
Yu-Pang <tedchen524@gmail.com>
2025-05-05 08:49:04 +0800

    ✨ Validating JSON input

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"maps"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -33,6 +36,54 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
+
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	// 1MB limit
+	r.Body = http.MaxBytesReader(w, r.Body, 1_048_576)
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
+	if err != nil {
+		// If there is an error during decoding, start the triage...
+		var syntaxErr *json.SyntaxError
+		var unmarshalTypeErr *json.UnmarshalTypeError
+		var invalidUnmarshalTypeErr *json.InvalidUnmarshalError
+		var maxBytesError *http.MaxBytesError
+
+		switch {
+		case errors.As(err, &syntaxErr):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxErr.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+		case errors.As(err, &unmarshalTypeErr):
+			if unmarshalTypeErr.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeErr.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeErr.Offset)
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown key %q", fieldName)
+		case errors.Is(err, maxBytesError):
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
+		case errors.As(err, &invalidUnmarshalTypeErr):
+			panic(err)
+		default:
+			return err
+		}
+	}
+	// If we got anything left in the body, it means that the client sent a JSON object with more than one top-level value.
+	// This is not allowed, so we return an error.
+	err = dec.Decode(&struct{}{})
+	if !errors.Is(err, io.EOF) {
+		return errors.New("body must only contain a single JSON value")
+	}
 
 	return nil
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,11 +6,38 @@ import (
 	"time"
 
 	"github.com/AlliesChen/greenlight-go/internal/data"
+	"github.com/AlliesChen/greenlight-go/internal/validator"
 )
 
 // POST /v1/movies
-func (app *application) craeteMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "create a new movie")
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
