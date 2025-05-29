7429851dd25f3f1435022164598a5cf4e6365d98
Yu-Pang <tedchen524@gmail.com>
2025-05-29 08:56:18 +0800

    🚧 Parsing query string parameters

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,9 +7,11 @@ import (
 	"io"
 	"maps"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
+	"github.com/AlliesChen/greenlight-go/internal/validator"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -86,4 +88,37 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 	}
 
 	return nil
+}
+
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+	return s
+}
+
+func (app *application) readCSV(qs url.Values, key string, defaultValue []string) []string {
+	csv := qs.Get(key)
+	if csv == "" {
+		return defaultValue
+	}
+	// Split the string by commas and return the resulting slice.
+	return strings.Split(csv, ",")
+}
+
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue
+	}
+
+	return i
 }
